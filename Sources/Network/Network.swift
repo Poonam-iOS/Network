b578a797ed0473d63d518b690982a161decbc534
Poonam-iOS <shapoonam302@gmail.com>
2026-01-19 22:07:53 +0530

    Initial Commit

---
+++ Sources/Network/Network.swift
@@ -0,0 +1,16 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+
+public class Network {
+    
+    var session: URLSession
+    
+    public init() {
+        session = URLSession.shared
+    }
+    
+    
+}
